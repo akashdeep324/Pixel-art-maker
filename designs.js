3f0a3611df69f5afbe7fa656f03d3b6edaa82991
akashdeep324 <akashdeep324@gmail.com>
2018-05-14 12:00:57 +0530

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,46 @@
 
 // When size is submitted by the user, call makeGrid()
 
+var submit;
+
+submit = jQuery('input[type="submit"]');
+
+submit.click(function(event) {
+    event.preventDefault();
+    makeGrid();
+});
+
+
+/**
+ * Make height x width grid
+ * @param {Number} height
+ * @param {Number} width
+ */
 function makeGrid() {
+    var pixelCanvas, position, gridHeight, gridWidth, row;
+    pixelCanvas = $('#pixel_canvas');
+    gridHeight = $('#input_height').val();
+    gridWidth = $('#input_width').val();
 
-// Your code goes here!
+    pixelCanvas.children().remove();
 
+    for (i = 0; i < gridHeight; i++) {
+        pixelCanvas.append('<tr></tr>');
+    }
+
+    row = $('tr');
+
+    for (j = 0; j < gridWidth; j++) {
+        row.append('<td></td>');
+    }
+
+    //checking the position and adding color to it
+    position = pixelCanvas.find('td');
+
+    position.click(function() {
+        var color;
+        color = $("#colorPicker").val();
+        $(this).attr('bgcolor', color);
+    });
+    
 }
